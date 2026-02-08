6cbe4df4d41ce1fee36b082a5dc5ad790be24962
SrijaBojanapalli <bojanapallysrija@gmail.com>
2026-02-08 18:48:37 +0530

    03-grade-calculator solution

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -26,4 +26,36 @@
  */
 export function calculateGrade(score, hasExtraCredit) {
   // Your code here
+  let grade = "INVALID";
+
+  if (score >= 0 && score <= 100 && hasExtraCredit) {
+    score = score + 5;
+
+    if (score > 100) {
+      score = 100;
+    }
+  } 
+
+
+
+  if (score >= 0 && score <= 59) {
+    grade = "F"
+  }
+  else if (score >= 60 && score <= 69) {
+    grade = "D"
+  }
+  else if (score >= 70 && score <= 79) {
+    grade = "C"
+  }
+  else if (score >= 80 && score <= 89) {
+    grade = "B"
+  }
+  else if (score >= 90 && score <= 100) {
+    grade = "A"
+  }
+  else {
+    grade = "INVALID";
+  }
+
+  return grade;
 }
